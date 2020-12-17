5cf6181598c1e6177b9bccbfe00cdd08495cbcdd
Harrison Zhu <hzhu359@gatech.edu>
2020-12-17 01:34:54 -0500

    update link

--- src/constants/data.js
+++ src/constants/data.js
@@ -5,7 +5,7 @@ export const data = {
     linkGitHub: "https://github.com/hzhu359",
     linkLinkedIn: "https://linkedin.com/in/hzhu359",
     linkInstagram: "https://www.instagram.com/harrisonfengzhu/",
-    linkResume: "https://ucf72ab095c27d90ec59b9f4b266.dl.dropboxusercontent.com/cd/0/inline2/BEu77x5mZcbb1-R4a7XJWoM9g5qU6_HYtufczVK2eAuxsPiQtfQmNxLH8OlK-P5L4bbczypCrb0s1D7ZvejsuLZJbANi1Q5SPa3eXyksKvSy0aYi1gkc1EuJw9b8LTl0SGVgsg62NDucf9nEmxVQ_XL8VP2ER51y_2HwGo9RABHXnlb3ZnjfFZvt_0MvGh96vZvXsoe7Jj-bBDIl6u5sJF1DdOp70K3knj2saFlcd_wA9qMxK4hFR_XyN7iFpmt-7ZyOwDKBk4EV0_cbOSF9FxHUrUwACqzD49gOUQ_ve3qG9J3PObFro4KujbDbiZKW8auAwfd7d0Av9BICg8JJwOJA4su-8kFzd5sEerk-Bn7DQQ/file",
+    linkResume: "https://www.dropbox.com/s/hwtjmsshm27xs81/Harrison_Zhu_resume.pdf?raw=1",
     linkProko: "https://youtu.be/-rQyZYPvVew",
     linkQuizBowl: "https://hdwhite.org/qb/stats/player/harrison+zhu/",
     greetList: [
